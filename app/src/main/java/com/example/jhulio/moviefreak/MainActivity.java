cacaeb1ab36495d04843965ff10b56c93b63e06b
Giulio Dalmasson <gdalmasson@gmail.com>
2016-01-20 21:22:50 +1100

    MoviesFragment Implemented

--- app/src/main/java/com/example/jhulio/moviefreak/MainActivity.java
+++ app/src/main/java/com/example/jhulio/moviefreak/MainActivity.java
@@ -2,15 +2,10 @@ package com.example.jhulio.moviefreak;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.GridView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private GridView moviesGridView;
-    private GridViewAdapter moviesGridAdapter;
-    private ArrayList<MoviesGridItem> moviesGridData;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
 
 
     @Override
@@ -18,13 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        moviesGridView = (GridView) findViewById(R.id.gridview_movies);
 
-        //Initialize the Grid view (with empty data)
-
-        moviesGridData = new ArrayList<>();
-        moviesGridAdapter = new GridViewAdapter(this, R.layout.grid_item_movies, moviesGridData);
-        moviesGridView.setAdapter(moviesGridAdapter);
 
     }
 }
---
+++ app/src/main/java/com/example/jhulio/moviefreak/MoviesFragment.java
@@ -0,0 +1,179 @@
+package com.example.jhulio.moviefreak;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class MoviesFragment extends Fragment {
+
+    //private OnFragmentInteractionListener mListener;
+    private static final String LOG_TAG = MoviesFragment.class.getSimpleName();
+
+    private GridView mGridView;
+    private GridViewAdapter mGridAdapter;
+    private ArrayList<MoviesGridItem> mGridData;
+    private String FEED_URL = "http://javatechig.com/?json=get_recent_posts&count=45";
+
+    public MoviesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
+
+        mGridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        mGridView.setAdapter(mGridAdapter);
+        //mGridView.setOnItemClickListener(mGridAdapter);
+        new AsyncHttpTask().execute(FEED_URL);
+        return rootView;
+
+    }
+
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            Integer result = 0;
+            try {
+                // Create Apache HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(params[0]));
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+
+                // 200 represents HTTP OK
+                if (statusCode == 200) {
+                    String response = streamToString(httpResponse.getEntity().getContent());
+                    parseResult(response);
+                    result = 1; // Successful
+                } else {
+                    result = 0; //"Failed
+                }
+            } catch (Exception e) {
+                Log.d(LOG_TAG, e.getLocalizedMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            // Download complete. Let us update UI
+            if (result == 1) {
+                mGridAdapter.setGridData(mGridData);
+            } else {
+                // Toast.makeText(MoviesFragment.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+        String streamToString(InputStream stream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            String result = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+
+            // Close stream
+            if (null != stream) {
+                stream.close();
+            }
+            return result;
+        }
+
+        /**
+         * Parsing the feed results and get the list
+         *
+         * @param result
+         */
+        private void parseResult(String result) {
+            try {
+                JSONObject response = new JSONObject(result);
+                JSONArray posts = response.optJSONArray("posts");
+                MoviesGridItem item;
+                for (int i = 0; i < posts.length(); i++) {
+                    JSONObject post = posts.optJSONObject(i);
+                    item = new MoviesGridItem();
+                    JSONArray attachments = post.getJSONArray("attachments");
+                    if (null != attachments && attachments.length() > 0) {
+                        JSONObject attachment = attachments.getJSONObject(0);
+                        if (attachment != null)
+                            item.setImage(attachment.getString("url"));
+                    }
+                    mGridData.add(item);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+    /*
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (activity instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) activity;
+        } else {
+            throw new RuntimeException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+    */
+
